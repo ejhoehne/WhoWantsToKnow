b56dfd2471c445091cd191f48c0ba25ae68056f2
Emily Hoehne <emilyhoehne@theglutconomist.home>
2014-09-23 18:58:35 -0600

    Step 1

---
+++ EJHViewController.h
@@ -0,0 +1,13 @@
+//
+//  EJHViewController.h
+//  WhoWantsToKnow
+//
+//  Created by Emily Hoehne on 9/23/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EJHViewController : UIViewController
+
+@end
--- WhoWantsToKnow/AppDelegate.m
+++ WhoWantsToKnow/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "EJHViewController.h"
 
 @implementation AppDelegate
 
@@ -14,6 +15,12 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    
+    EJHViewController *newController = [EJHViewController new];
+    UINavigationController *navController = [[UINavigationController alloc]initWithRootViewController:newController];
+    self.window.rootViewController = navController;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
